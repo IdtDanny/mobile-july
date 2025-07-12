450cd4a8f3adbc59d0855598fc4c7f1179985219
Danny I <idtdanny@gmail.com>
2025-07-12 22:20:55 +0200

    TaskFlow completed

--- taskflow/lib/homepage.dart
+++ taskflow/lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:taskflow/widgets/task_item.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,7 +8,18 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(elevation: 2, title: Text('TaskFlow Home')),
-      body: Center(child: Text('No tasks yet..')),
+
+      // body: Center(child: Text('No tasks yet..')),
+      body: ListView(
+        children: const [
+          SizedBox(height: 8),
+          TaskItem(title: 'Finished Module of Flutter Course'),
+          TaskItem(title: 'Submit the weekly report'),
+          TaskItem(title: 'Call the client Back'),
+          TaskItem(title: 'Grocery shopping for the week'),
+          TaskItem(title: 'Plan the weekend trip'),
+        ],
+      ),
     );
   }
 }
